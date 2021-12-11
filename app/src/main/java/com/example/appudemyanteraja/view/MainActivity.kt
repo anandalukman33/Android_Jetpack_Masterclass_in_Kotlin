132043bd4d8f8cd85e21f3daf52d920aea19ea71
Ananda Muhamad Lukman <ananda.muhamad.lukman@gmail.com>
2021-12-11 21:12:29 +0700

    Navigation Setup

--- app/src/main/java/com/example/appudemyanteraja/view/MainActivity.kt
+++ app/src/main/java/com/example/appudemyanteraja/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.appudemyanteraja
+package com.example.appudemyanteraja.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appudemyanteraja.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
