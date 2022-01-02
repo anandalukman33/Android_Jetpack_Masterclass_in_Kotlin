476ab079d59d89c2828e52c4423433684428fdf4
Ananda Muhamad Lukman <ananda.muhamad.lukman@gmail.com>
2022-01-03 02:57:18 +0700

    adding ActionBar Back with navController, and adding logic into 3 activity detail, list fragment and mainActivity

--- app/src/main/java/com/example/appudemyanteraja/view/DetailFragment.kt
+++ app/src/main/java/com/example/appudemyanteraja/view/DetailFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.appudemyanteraja.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class DetailFragment : Fragment() {
 
@@ -14,5 +17,13 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        float_detail.setOnClickListener {
+            val jumpAction = DetailFragmentDirections.actionDetailFragmentToListFragment()
+            Navigation.findNavController(it).navigate(jumpAction)
+        }
     }
 }
--- app/src/main/java/com/example/appudemyanteraja/view/ListFragment.kt
+++ app/src/main/java/com/example/appudemyanteraja/view/ListFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.appudemyanteraja.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -14,5 +17,13 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        float_list.setOnClickListener {
+            val jumpAction = ListFragmentDirections.actionListFragmentToDetailFragment()
+            Navigation.findNavController(it).navigate(jumpAction)
+        }
     }
 }
