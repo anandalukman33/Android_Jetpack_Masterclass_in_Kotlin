5998264e8ef8aa130e3ccfcee735b80b55cc1e08
Ananda Lukman <ananda.muhamad.lukman@gmail.com>
2022-01-13 14:36:05 +0700

    Setup Modeling for Database Room

--- app/src/main/java/com/example/appudemyanteraja/constant/DogConstant.kt
+++ app/src/main/java/com/example/appudemyanteraja/constant/DogConstant.kt
@@ -1,6 +1,6 @@
 package com.example.appudemyanteraja.constant
 
-object UrlConstant {
+object DogConstant {
 
     // Url Api
     const val BASE_URL = "https://raw.githubusercontent.com"
@@ -14,4 +14,12 @@ object UrlConstant {
     const val BRED_FOR = "bred_for"
     const val TEMPERAMENT = "temperament"
     const val URL = "url"
+
+    // Key Database
+    const val BREED_ID = "breed_id"
+    const val DOG_NAME = "dog_name"
+    const val LIFE_SPANS = "life_span"
+    const val BREEDS_GROUP = "breed_group"
+    const val BREEDS_FOR = "bred_for"
+    const val DOG_URL = "dog_url"
 }
--- app/src/main/java/com/example/appudemyanteraja/model/DogBreed.kt
+++ app/src/main/java/com/example/appudemyanteraja/model/DogBreed.kt
@@ -1,35 +1,53 @@
 package com.example.appudemyanteraja.model
 
-import com.example.appudemyanteraja.constant.UrlConstant.BRED_FOR
-import com.example.appudemyanteraja.constant.UrlConstant.BREED_GROUP
-import com.example.appudemyanteraja.constant.UrlConstant.ID
-import com.example.appudemyanteraja.constant.UrlConstant.LIFE_SPAN
-import com.example.appudemyanteraja.constant.UrlConstant.NAME
-import com.example.appudemyanteraja.constant.UrlConstant.TEMPERAMENT
-import com.example.appudemyanteraja.constant.UrlConstant.URL
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.appudemyanteraja.constant.DogConstant.BRED_FOR
+import com.example.appudemyanteraja.constant.DogConstant.BREEDS_FOR
+import com.example.appudemyanteraja.constant.DogConstant.BREEDS_GROUP
+import com.example.appudemyanteraja.constant.DogConstant.BREED_GROUP
+import com.example.appudemyanteraja.constant.DogConstant.BREED_ID
+import com.example.appudemyanteraja.constant.DogConstant.DOG_NAME
+import com.example.appudemyanteraja.constant.DogConstant.DOG_URL
+import com.example.appudemyanteraja.constant.DogConstant.ID
+import com.example.appudemyanteraja.constant.DogConstant.LIFE_SPAN
+import com.example.appudemyanteraja.constant.DogConstant.LIFE_SPANS
+import com.example.appudemyanteraja.constant.DogConstant.NAME
+import com.example.appudemyanteraja.constant.DogConstant.TEMPERAMENT
+import com.example.appudemyanteraja.constant.DogConstant.URL
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class DogBreed (
 
+    @ColumnInfo(name = BREED_ID)
     @SerializedName(ID)
     val breedId: String? = null,
 
+    @ColumnInfo(name = DOG_NAME)
     @SerializedName(NAME)
     val dogBreed: String? = null,
 
+    @ColumnInfo(name = LIFE_SPANS)
     @SerializedName(LIFE_SPAN)
     val lifeSpan: String? = null,
 
+    @ColumnInfo(name = BREEDS_GROUP)
     @SerializedName(BREED_GROUP)
     val breedGroup: String? = null,
 
+    @ColumnInfo(name = BREEDS_FOR)
     @SerializedName(BRED_FOR)
     val bredFor: String? = null,
 
     @SerializedName(TEMPERAMENT)
     val temperament: String? = null,
 
+    @ColumnInfo(name = DOG_URL)
     @SerializedName(URL)
     val imageUrl: String? = null
-
-)
+) {
+    @PrimaryKey(autoGenerate = true)
+    var uuid: Int = 0
+}
